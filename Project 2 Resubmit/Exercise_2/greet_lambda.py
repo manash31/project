22c19115262407dafe389b912028b93e0717d41f
Manash Das <manash31@gmail.com>
2020-08-23 09:57:19 -0400

    2nd submission

--- Project 2 Resubmit/Exercise_2/greet_lambda.py
+++
@@ -1,8 +0,0 @@
-import os
-
-def lambda_handler(event, context):
-    message = "{} from Lambda!" 
-
-    return { 
-        'message' : message
-    } 
--- Project 2 Resubmit/Exercise_2/variables.tf
+++
@@ -1,4 +0,0 @@
-# TODO: Define the variable for aws_region
-variable "aws_region" {
-  default = "us-east-1"
-}
